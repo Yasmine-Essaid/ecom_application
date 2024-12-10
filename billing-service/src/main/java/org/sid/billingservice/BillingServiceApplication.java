d5b204675451d5c2d74f843a7c5a0fc085ee83ee
YasmineEssaid <YasmineEssaid@YASMINE-ESSAID>
2024-12-10 09:40:38 +0100

    V4

--- billing-service/src/main/java/org/sid/billingservice/BillingServiceApplication.java
+++ billing-service/src/main/java/org/sid/billingservice/BillingServiceApplication.java
@@ -13,10 +13,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
-
 import java.util.Collection;
 import java.util.Date;
-import java.util.List;
 import java.util.Random;
 
 @SpringBootApplication
